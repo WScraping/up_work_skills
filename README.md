df8cf4ca41821f04a51f097ed4284ccc821f5e54
Maxim Dzhuliy <mak7.dj@gmail.com>
2015-12-05 20:17:58 -0500

    Fix selenium parser and readme

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ Just for fun, maybe it will be helpful for someone :smile:
 Very simple:
 
 ```ruby
-skill_parser = SkillParser.new ('a'..'z')
+skill_parser = Parsers::UpWork::SkillsParser.new('a'..'z')
 skill_parser.parse_pages
-p skill_parser.data #=> Array[..]
+skill_parser.data #=> Array[..]
 ```
 
 ## Contributing
 
-1. Fork it ( https://github.com/max-si-m/up_work_skills/fork )
+1. Fork it ( https://github.com/WScraping/up_work_skills )
 2. Create your feature branch (`git checkout -b my-new-feature`)
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
--- selenium_parser.rb
+++ selenium_parser.rb
@@ -1,34 +1,34 @@
 require 'rubygems'
 require 'selenium-webdriver'
 
-# Parser class
-class SkillParser
-  attr_accessor :pages, :browser, :data
 
-  # pages is array or range of pages
-  def initialize(pages)
-    raise ArgumentError.new('Only array, or range') unless pages.is_a?(Array) || pages.is_a?(Range)
-    @pages = pages || ('a'..'z')
-    @browser = Selenium::WebDriver.for :firefox
-    @data = []
-  end
+# skill_parser = Parsers::UpWork::SkillsParser.new('a'...'c')
+# skill_parser.parse_pages
+# p skill_parser.data
 
-  # just parser
-  def parse_pages
-    @pages.each do |page|
-      begin
-        @browser.get "https://www.upwork.com/i/freelancer-skills-#{page}/"
-        links = @browser.find_elements(:xpath,"//*[contains(@class, 'skill-item')]/a").map(&:text)
-        @data.concat(links)
-      rescue StandartError => e
-        puts "Some errors: #{e}"
+module Parsers
+  module UpWork
+    class SkillsParser
+      attr_accessor :pages, :browser, :data
+
+      # pages is array or range of pages
+      def initialize(pages)
+        fail ArgumentError 'Only array, or range' unless
+          pages.is_a?(Array) || pages.is_a?(Range)
+        @pages = pages || ('a'..'z')
+        @browser = Selenium::WebDriver.for :firefox
+        @data = []
+      end
+
+      def parse_pages
+        @pages.each do |page|
+          @browser.get "https://www.upwork.com/i/freelancer-skills-#{page}/"
+          links_xpath = "//*[contains(@class, 'skill-item')]/a"
+          links = @browser.find_elements(:xpath, links_xpath).map(&:text)
+          @data.concat(links)
+        end
+        @browser.quit
       end
     end
-    @browser.quit
   end
 end
-
-#skill_parser = SkillParser.new ('a'..'k')
-#skill_parser.parse_pages
-#p skill_parser.data
-
