2db4b7918dd581997b4968cb8948752cc2007a54
Maxim Dzhuliy <mak7.dj@gmail.com>
2015-12-05 19:54:47 -0500

    Fix nokogiri parser

--- parser.rb
+++ parser.rb
@@ -5,37 +5,35 @@ require 'nokogiri'
 # Class for parsing skills on UpWork
 # Simple usage :)
 
-# skill_parser = SkillParser.new ('a'..'z')
+# skill_parser = Parsers::UpWork::SkillsParser.new ('a'..'z')
 # skill_parser.parse_pages
 # p skill_parser.data
 
-class SkillParser
-  attr_accessor :pages, :data
+module Parsers
+  module UpWork
+    class SkillsParser
+      attr_accessor :pages, :data
 
-  # pages is array or range of pages 
-  def initialize pages
-    raise ArgumentError.new('Only array, or range') unless pages.kind_of?(Array) ||  pages.kind_of?(Range)
-    @pages = pages || ('a'..'z')
-    @data = []
-  end
+      # pages is array or range of pages
+      def initialize(pages)
+        fail ArgumentError 'Only array, or range' unless
+          pages.is_a?(Array) || pages.is_a?(Range)
+        @pages = pages || ('a'..'z')
+        @data = []
+      end
 
-  # just parser
-  def parse_pages
-    @pages.each do |page|     
-      begin
-        uri = URI("https://www.upwork.com/i/freelancer-skills-#{page}/")
-        doc = Nokogiri::HTML(Net::HTTP.get(uri)) do |config|
-            config.strict.nonet.noblanks.noerror 
-        end
+      def parse_pages
+        @pages.each do |page|
+          uri = URI("https://www.upwork.com/i/freelancer-skills-#{page}/")
+          doc = Nokogiri::HTML(Net::HTTP.get(uri)) do |config|
+            config.strict.nonet.noblanks.noerror
+          end
 
-        doc.css('.skill-item').each do |list|
-          skill = list.css('a').text
-          unless skill.nil?          
-            @data << skill
+          doc.css('.skill-item').each do |list|
+            skill = list.css('a').text
+            @data << skill if skill
           end
         end
-      rescue Exception => e
-        puts "Some errors: #{e}"
       end
     end
   end
